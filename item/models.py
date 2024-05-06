89002808fea9a7a6bbd2d7949e309a67127d143a
Ahmedgamalsoli <121308198+Ahmedgamalsoli@users.noreply.github.com>
2024-05-06 17:01:49 +0300

    Add files via upload

---
+++ item/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.core.files import File
+from django.contrib.auth.models import User
+
+from io import BytesIO
+# Create your models here.
+ 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+   
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+        category = models.ForeignKey(Category, related_name='items' , on_delete=models.CASCADE)
+        name = models.CharField(max_length=255)
+        description = models.TextField(blank=True,null=True)
+        price = models.FloatField()
+        image = models.ImageField(upload_to='item_images', blank=True,null=True)
+        is_sold = models.BooleanField(default=False)
+        created_by = models.ForeignKey(User , related_name='items' , on_delete=models.CASCADE)
+        created_at = models.DateTimeField(auto_now_add=True)
+        class Meta:
+                ordering = ('-created_at',)
+            
+        def __str__(self):
+            return self.name
+
+        def get_display_price(self):
+            return self.price / 100
+        
+        def get_thumbnail(self):
+            if self.thumbnail:
+                return self.thumbnail.url
+            else:
+                if self.image:
+                    self.thumbnail = self.make_thumbnail(self.image)
+                    self.save()
+
+                    return self.thumbnail.url
+                else:
+                    return 'https://via.placeholder.com/240x240x.jpg'   
+            
---
+++ my_app/urls.py
@@ -0,0 +1,39 @@
+"""
+URL configuration for my_app project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from item.views import Item
+from cart.views import  CartItemView,checkout ,add_to_cart
+from cart.models import CartItem,Cart
+
+urlpatterns = [
+    path('', include('core.urls')),
+    #path('', include('cart.urls')),
+    path('cart/',CartItem,name='cart'),
+    path('cart/checkout/',checkout,name='checkout'),
+    path('add_to_cart/<int:product_id>/',add_to_cart,name='add_to_cart'),
+    path('add_cart',CartItemView.as_view(),name='CartItemView'),
+    path('items/', include('item.urls')), 
+    path('inbox/', include('conversation.urls')),
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL , document_root =settings.MEDIA_ROOT)
+
+admin.site.site_header = "NA Store Admin"
+admin.site.site_title = "NA Store"
+admin.site.index_title = "Welcome to NA Store administrator"
